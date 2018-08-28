608dc80e450eaf1e2c6fabef524b4de7cd0ca701
Glitch (tranquil-reptile) <none>
2018-08-28 21:42:59 +0000

    🎊👐 Checkpoint ./data-layer.js:1433977/216

--- data-layer.js
+++ data-layer.js
@@ -4,7 +4,7 @@ var exists = fs.existsSync(dbFile);
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database(dbFile);
 
-var createScript = 
+var createScript = 'CREATE TABLE Users (username nvarchar(50))');
 
 var dataLayer = {
   setup : function(dbFile)
@@ -12,12 +12,12 @@ var dataLayer = {
     // if ./.data/sqlite.db does not exist, create it, otherwise print records to console
     db.serialize(function(){
     if (!exists) {
-      db.run('CREATE TABLE Dreams (dream TEXT)');
-      console.log('New table Dreams created!');
+      db.run(createScript);
+      console.log('New table Users created!');
 
       // insert default dreams
       db.serialize(function() {
-        db.run('INSERT INTO Dreams (dream) VALUES ("Find and count some sheep"), ("Climb a really tall mountain"), ("Wash the dishes")');
+        db.run('INSERT INTO Users (dream) VALUES ("Find and count some sheep"), ("Climb a really tall mountain"), ("Wash the dishes")');
       });
     }
     else {
