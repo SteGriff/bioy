1410056f942ca5b1be4cb30a8879eb9e67bf56c3
Glitch (bioy) <none>
2018-09-03 19:03:36 +0000

    💏✋ Checkpoint ./server.js:1433977/3387 ./sql/create-users.sql:1433977/299

--- server.js
+++ server.js
@@ -20,40 +20,115 @@ var exists = fs.existsSync(dbFile);
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database(dbFile);
 
-// if ./.data/sqlite.db does not exist, create it, otherwise print records to console
-// db.serialize(function(){
-//   if (!exists) {
-//     db.run('CREATE TABLE Dreams (dream TEXT)');
-//     console.log('New table Dreams created!');
-    
-//     // insert default dreams
-//     db.serialize(function() {
-//       db.run('INSERT INTO Dreams (dream) VALUES ("Find and count some sheep"), ("Climb a really tall mountain"), ("Wash the dishes")');
-//     });
-//   }
-//   else {
-//     console.log('Database "Dreams" ready to go!');
-//     db.each('SELECT * from Dreams', function(err, row) {
-//       if ( row ) {
-//         console.log('record:', row);
-//       }
-//     });
-//   }
-// });
-
-// http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-// endpoint to get all the dreams in the database
-// currently this is the only endpoint, ie. adding dreams won't update the database
-// read the sqlite3 module docs and try to add your own! https://www.npmjs.com/package/sqlite3
 app.get('/getReadings', function(request, response) {
   db.all('SELECT * from Readings', function(err, rows) {
     response.send(JSON.stringify(rows));
   });
 });
+
+
+app.post('/createUser', function(request, response) {
+  ifAuthenticated(request.body, function(error, exists)
+  {
+    console.log(error, exists);
+    if (exists)
+    {
+      response.status(400).send("Already exists");
+      return;
+    }
+    var hash = bcrypt.hashSync(request.body.password, 10);
+    db.run("insert into Users (Username, Password) values (?, ?)", [request.body.username, hash], function(error)
+    {
+      if (error) {
+        console.log("Fail", error);
+        response.status(500).send("Insert failed");
+      }
+      else { 
+        response.sendStatus(201);
+      }
+    });
+  });
+});
+
+app.post('/addMessage', function(request, response) {
+  ifAuthenticated(request.body, function(error, userId){
+    if (error)
+    {
+      response.status(401).send("Wrong username/password!");
+    }
+    else
+    {
+      db.run("insert into Messages (UserID, Message) values(?, ?)", [userId, request.body.message], function(error)
+      {
+        if (error) {
+          console.log("Fail", error);
+          response.status(500).send("Insert failed");
+        }
+        else { 
+          response.sendStatus(201);
+        }
+      });
+    }
+  });
+});
+
+app.get('/getMessages', function(request, response) {
+  ifAuthenticated(request.query, function(error, userId){
+    if (error)
+    {
+      console.log("Fail", error);
+      response.status(401).send("Wrong username/password!");
+    }
+    else
+    {
+      console.log("Getting msgs");
+      db.all('SELECT * from Messages where UserID=?',[userId], function(err, rows) {
+        console.log(err, rows);
+        response.send(JSON.stringify(rows));
+      });
+    }
+  });
+});
+
+function ifAuthenticated(data, callback)
+{
+  if (!data || !data.username || !data.password)
+  {
+    callback("Missing parameters. Got: ", data);
+    return;
+  }
+  db.get("select * from Users where Username=?", [data.username], function(error,row)
+  {
+    if (!row || !row.Password)
+    {
+      console.log("Unauthorised");
+      callback("Unauthorised", false);
+      return;
+    }
+    bcrypt.compare(data.password, row.Password)
+    .then(function(isMatch){
+      if (isMatch)
+      {
+        console.log("Authorised", row);
+        callback(null, row.ID);
+      }
+      else
+      {
+        console.log("Unauthorised");
+        callback("Unauthorised", true);
+      }
+    })
+    .catch(function(error){
+      console.log("Auth error", error);
+      callback(error);
+    });
+  });
+}
+
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function() {
--- sql/create-users.sql
+++ sql/create-users.sql
@@ -1,5 +1,13 @@
 create table Users 
 (
-	Username nvarchar(100) PRIMARY KEY,
-  Password 
+  ID integer primary key autoincrement,
+	Username nvarchar(100),
+  Password nvarchar(100)
+);
+
+create table Messages
+(
+  ID integer primary key autoincrement,
+  UserID integer,
+  Message nvarchar(255)
 );
