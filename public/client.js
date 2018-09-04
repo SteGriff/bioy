ca8c152ba944f8978c910a1037904a2d11644513
Glitch (bioy) <none>
2018-09-04 09:58:58 +0000

    🏢🌖 Checkpoint ./public/client.js:1433977/403 ./views/index.html:1433977/536

--- public/client.js
+++ public/client.js
@@ -42,9 +42,14 @@ var app = new Vue({
       var formData = self.newRequest();
       console.log(formData);
       $.get('/getNotes', formData, function(response){
-        console.log(response); 
+        //console.log(response); 
         self.notes = JSON.parse(response);
-        console.log(self.notes);
+        for(var nx in self.notes)
+        {
+          var note = self.notes[nx];
+          console.log(note);
+        }
+        //console.log(self.notes);
         self.message="Notes loaded";
         self.loggedIn = true;
       })
@@ -58,13 +63,14 @@ var app = new Vue({
       self=this;
       self.message="Saving...";
       var formData = self.newRequest();
-      formData['field'] = self.modalField;
+      formData['day'] = self.activeReading.Day;
       formData['note'] = self.activeReading.GeneralNote;
       formData['done'] = self.activeReading.Done;
       console.log(formData);
       $.post('/saveNote', formData, function(response){
         console.log(response); 
         self.message="Saved!";
+        self.closeModal();
       })
       .fail(function(data){
         self.showMessage(data.responseText);
@@ -83,18 +89,20 @@ var app = new Vue({
     },
     openModal: function(reading)
     {
-      console.log("openModal", reading)
       this.activeReading = reading;
       this.modal = true;
     },
     closeModal : function()
     {
-      console.log("closeModal");
       this.modal = false;
     },
     summary : function(reading)
     {
       return reading.OT + "; " + reading.NT + "; " + reading.PP; 
+    },
+    truncate : function(note)
+    {
+      return note.subscript(0,100); 
     }
   }
 });
