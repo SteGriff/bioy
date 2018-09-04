b098c29b9f06794c69ccfdcc41d82f534f5fcc23
Glitch (bioy) <none>
2018-09-04 09:05:16 +0000

    🏪🎬 Checkpoint ./public/synced-tick.js:1433977/346

--- public/synced-tick.js
+++ public/synced-tick.js
@@ -1,6 +1,19 @@
-
+Vue.component('synced-tick', {
+  props: ['value', 'loggedIn', 'reading'],
+  template: `<div>
+			<label :for="name">
+				{{name}}
+			</label>
+			<input
+				:id="name"
+				:name="name"
+				:value="value"
+				v-on:input="$emit('input', $event.target.value)"/>
+			</div>`
+});
 
 `<input type="checkbox" name="done"
- v-if="loggedIn"
- v-model="reading.Done"
- v-on:click="setDone(reading)">`
+v-if="loggedIn"
+v-model="reading.Done"
+v-on:click="setDone(reading)"
+v-on:input="$emit('input', $event.target.value)>`
